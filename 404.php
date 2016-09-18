6a6afd29ca650adee5dba13add424f29753b6bb7
avijitnandy <avijitnandy@users.noreply.github.com>
2016-09-18 17:52:24 +0600

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header();?>
+<section>
+	<hr class="no-margin" />
+		
+		<div class="middle-container section-content">
+			<div class="container box section-content align-center contact-page">
+				<h2>Oops.Page Not Found!</h2>
+				<p>It seems you're looking for something that isn't here.</p>
+				<p>Perhaps try the <a href="<?php echo home_url();?>">HOMEPAGE</a>?</p>
+			</div>
+		</div>
+</section>
+<?php get_footer();?>
